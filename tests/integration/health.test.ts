6ce79283c7f500a8160236d99ea5c9299a41f3a3
Lucas Viana <lucasvfv@gmail.com>
2022-11-15 10:43:01 -0300

    Initial commit

---
+++ tests/integration/health.test.ts
@@ -0,0 +1,14 @@
+////import app from '@/app';
+//import supertest from 'supertest';
+
+
+//const server = supertest(app);
+
+//describe('GET /health', () => {
+//	it('should respond with status 200 with OK! text', async () => {
+//		const response = await server.get('/health');
+
+//		expect(response.status).toBe(200);
+//		expect(response.text).toBe('OK!');
+//	});
+//});
